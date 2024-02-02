edf4a2d11ee2120acad192034ad442803a3e1914
PriyankMarkana <cdmi.priyankmarkana48@gmail.com>
2024-02-02 18:41:02 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+function App(){
 
-function App() {
+  let [sign,setsign]=useState("");
+  let [temp,settemp]=useState("");
+  let [fval,fsetval]=useState("");
+  let [del,setdel]=useState("");
+  
+    let Ans=(a)=>{
+	  settemp(temp+a);
+    }
+
+	let fdata=(s)=>{
+		setsign(s);
+		fsetval(temp)
+		settemp('')
+	}
+
+	let ldata=()=>{
+
+		if(sign=='+')
+		{
+			 settemp(`${parseInt(fval)+parseInt(temp)}`);
+		}
+		if(sign=='-')
+		{
+			settemp(`${parseInt(fval)-parseInt(temp)}`);
+		}
+		if(sign=='*')
+		{
+			settemp(`${parseInt(fval)*parseInt(temp)}`);
+		}
+		if(sign=='/')
+		{
+			if(temp==0){
+				settemp('can not divide to 0');
+			}
+			else{
+			settemp(`${parseInt(fval)/parseInt(temp)}`);
+			}
+		}
+		if(sign=="%")
+		{
+			settemp(`${parseInt(fval)%parseInt(temp)}`);
+		}
+	}
+
+	const clr=(s)=>{
+		if(s=='c'){
+					settemp("");
+				}
+				else{
+					
+					settemp(temp.slice(0, -1));
+				}
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="main">
+		<div className="ans">
+			<input type="text" value={temp} id="ans2"></input>
+		</div>
+		<div class="first">
+			<input type="button" name="" id="C" value="C"  onClick={()=>clr("c")}></input>
+			<input type="button" name="" id="Del" value="Del"onClick={()=>clr("d")}></input>
+			<input type="button" name="" id="%" value="%"  onClick={() => fdata('%')}></input>
+			<input type="button" name="" id="/" value="/"  onClick={() => fdata("/")}></input>
+		</div>
+		<div class="first">
+			<input type="button" name="" id="7" value="7" onClick={() => Ans(7)}></input>
+			<input type="button" name="" id="8" value="8" onClick={() => Ans(8)}></input>
+			<input type="button" name="" id="9" value="9" onClick={() => Ans(9)}></input>
+			<input type="button" name="" id="X" value="X" onClick={() => fdata('*')}></input>
+		</div>
+		<div class="first">
+			<input type="button" name="" id="4" value="4" onClick={() => Ans(4)}></input>
+			<input type="button" name="" id="5" value="5" onClick={() => Ans(5)}></input>
+			<input type="button" name="" id="6" value="6" onClick={() => Ans(6)}></input>
+			<input type="button" name="" id="-" value="-"  onClick={() => fdata('-')}></input>
+		</div>
+		<div class="first">
+			<input type="button" name="" id="1" value="1" onClick={() => Ans(1)}></input>
+			<input type="button" name="" id="2" value="2" onClick={() => Ans(2)}></input>
+			<input type="button" name="" id="3" value="3" onClick={() => Ans(3)}></input>
+			<input type="button" name="" id="+" value="+"  onClick={() => fdata('+')}></input>
+		</div>
+		<div class="first">
+			<input type="button" name="" id="0" value="0"  onClick={() => Ans(0)}></input>
+			<input type="button" name="" id="." value="." onClick={() => Ans('.')}></input>
+			<input type="button" name="" id="=" value="="  onClick={()=>ldata()}></input>
+		</div>
+	</div>
+    </>
   );
 }
 
